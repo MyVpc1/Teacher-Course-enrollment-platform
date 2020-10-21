231cc8ebc59a2a01bb34b1b0a322bfb10029f10f
Manish Manjul <73165500+manishmanjul77@users.noreply.github.com>
2020-10-21 14:53:31 +0530

    Update db_config.class.php

--- class/db_config.class.php
+++ class/db_config.class.php
@@ -6,13 +6,13 @@
         public static $interval = 0;
         public static $DIR = "https://fyores.com";
         public static $GMAIL = "info@fyores.com";
-        public static $GMAIL_PASSWORD = "+Xc7,ofT~ypo";
+        public static $GMAIL_PASSWORD = "";
 
         public static function _DB()
         {
             try
             {
-                self::$database = new PDO('mysql:host=localhost; dbname=meemabvx_fyores; charset=utf8mb4', 'meemabvx_fyores77', 'dfbX4_EsMOX@');
+                self::$database = new PDO('mysql:host=localhost; dbname=fyores; charset=utf8mb4', '', '');
                 self::$database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 $e = self::$e;
             }
